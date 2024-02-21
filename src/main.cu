998a61073905a186218e5de7512b6e7b181461b8
Petro Mozil <mozil.petryk@gmail.com>
2024-02-21 17:07:51 +0200

    Fix stuff

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,17 @@
+#include "particle.h"
 #include <cstdio>
 
 int main() {
   printf("Hi there!\n");
+
+  Particle p1{
+      1.0F, {}, {}, 0L, 0L, nullptr,
+  };
+  Particle p2{
+      1.0F, {}, {}, 0L, 0L, nullptr,
+  };
+
+  printf("P1 intersects P2: %i\n", p1.intersects(p2));
 
   return 0;
 }
--- src/particle.cu
+++ src/particle.cu
@@ -2,5 +2,5 @@
 
 __host__ __device__ bool Particle::intersects(Particle const &rhs) {
   double const diameter = radius + rhs.radius;
-  return vec_3d_distance_squared(pos, rhs.pos) > diameter * diameter;
+  return vec_3d_distance_squared(pos, rhs.pos) < diameter * diameter;
 }
