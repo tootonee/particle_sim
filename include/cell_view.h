9621a48da992878c919eb2389c023be214363ef1
Petro Mozil <mozil.petryk@gmail.com>
2024-02-25 15:57:06 +0200

    Add header for cell view

--- include/cell_view.h
+++ include/cell_view.h
@@ -17,20 +17,28 @@ struct __align__(32) cell_view_t {
     size_t cells_per_axis;
 };
 
+struct __align__(32) cell_block_t {
+    cell_t cells[8];
+    size_t num_cells
+};
+
 void cell_view_init_host(cell_view_t &view, particle_box_t box,
                          size_t const cells_per_axis);
-void cell_view_add_particle_host(cell_view_t &view, size_t num_cells);
+void cell_view_add_particle_host(cell_view_t &view, particle_t const p);
 void cell_view_upscape_host(cell_view_t &view, size_t scale);
 
-__host__ __device__ cell_t *cell_view_get_cell(cell_view_t *view,
-                                               particle_t const &particle);
-__host__ __device__ void cell_view_get_particle_idx(cell_view_t *view,
-                                                    particle_t const &particle);
-
-__device__ cell_view_t cell_view_init_device(cell_view_t const &view);
+__device__ cell_view_t cell_view_device_from_host_obj(cell_view_t const &view);
 __device__ void cell_view_add_particle_device(cell_view_t &view,
-                                              particle_box_t *box,
-                                              size_t num_cells);
-__device__ void cell_view_upscape_device(cell_view_t &view, size_t scale);
+                                              particle_t const p);
+__device__ void cell_view_upscape_device(cell_view_t &view, size_t const scale);
+
+__host__ __device__ size_t cell_view_get_particle_idx(cell_t const &cell,
+                                               particle_t const &p);
+__host__ __device__ size_t cell_view_get_cell_idx(cell_view_t const &view,
+                                               particle_t const &p);
+__host__ __device__ cell_block_t cell_view_get_particle_neighbourhood(cell_view_t *view,
+                                                    particle_t const &p);
+__host__ __device__ bool particle_intersects(cell_view_t const &view,
+                                                particle_t const &p);
 
 #endif
