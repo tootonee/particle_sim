9bf62655a1ccaedd91864b25d6d334c7a09c3542
Petro Mozil <mozil.petryk@gmail.com>
2024-03-04 15:51:08 +0200

    Add particle movement sim
    Signed-off-by: Petro Mozil <mozil.petryk@gmail.com>

--- src/main.cu
+++ src/main.cu
@@ -3,9 +3,11 @@
 #include "particle_box.h"
 #include "pdb_export.h"
 
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <random>
-#include <stdio.h>
+#include <sstream>
 
 // __global__ void print_particle(particle_box_t box) {
 //   int id = threadIdx.x;
@@ -13,6 +15,11 @@
 //   printf("(%lf, %lf, %lf)\n", v.x, v.y, v.z);
 //   // cudaFree(box.particles[id].patches);
 // }
+
+constexpr size_t ITERATIONS = 1'000'000;
+constexpr size_t ITERATIONS_PER_EXPORT = 1'000;
+constexpr double TEMPERATURE = 2.0F;
+constexpr double BOLTZMANN_C = 1.380649e-23;
 
 int main() {
   std::random_device r;
@@ -29,7 +36,31 @@ int main() {
     view.add_particle_random_pos(0.5, unif_x, unif_y, unif_z, re);
   }
 
-  export_particles_to_pdb(view.box, "stochastic.pdb");
+  std::uniform_real_distribution<double> unif_r(0, 0.999F);
+  for (size_t iters = 0; iters <= ITERATIONS; iters++) {
+    if (iters % ITERATIONS_PER_EXPORT == 0) {
+      const size_t idx = iters / ITERATIONS_PER_EXPORT;
+      char buf[16];
+      std::sprintf(buf, "data/%06li.pdb", idx);
+      export_particles_to_pdb(view.box, buf);
+    }
+    size_t const p_idx = unif_r(re) * view.box.particle_count;
+    double const radius = view.box.particles[p_idx].radius;
+    double3 const old_pos = view.box.particles[p_idx].pos;
+
+    double old_energy = view.particle_energy_square_well(old_pos, radius);
+
+    double3 const new_pos = view.try_random_particle_disp(p_idx, unif_r, re);
+    if (new_pos.x == -1) {
+      continue;
+    }
+    double new_energy = view.particle_energy_square_well(new_pos, radius);
+    double prob = (old_energy - new_energy) / (BOLTZMANN_C * TEMPERATURE);
+    if (unif_r(re) >= prob) {
+      continue;
+    }
+  }
+
   view.free();
 
   return 0;
